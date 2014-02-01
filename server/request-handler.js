40cc4547f58a2e8e73d8a208d09a456c42358ee4
Hack Reactor Students <c.hughes87@gmail.com>
2014-02-01 15:54:25 -0800

    working out DB interface

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,53 +1,78 @@
-var _storage = [];
-// var logFile = "./log.txt";
 var fs = require('fs');
 var url = require('url');
+var Q = require('q');
 var db = require('./database');
-
-exports.loadMessages = function(){
-  // fs.readFile(logFile, {encoding: 'utf8'}, function(err,data){
-  //   if(err) throw err;
-  //   console.log(data);
-  // });
-};
 
 exports.handleRequest = function(request, response) {
   var statusCode = 200;
   var requestJSON = url.parse(request.url,true,true);
   if(requestJSON.pathname.match(/\/classes\//) === null){
     statusCode = 404;
-  }else if(request.method === "POST") {
-    statusCode = 201;
-    request.on('data', function(data){
-      postDataToDatabase(JSON.parse(data));
-      // var ws = fs.createWriteStream(logFile,{flags:'w'});
-      // _storage.push(JSON.parse(data));
-      // ws.write(data);
-    });
-  }else if(request.method === "GET") {
-    statusCode = 200;
   }
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = "application/json";
-  response.writeHead(statusCode, headers);
-  response.end(new Buffer(JSON.stringify(_storage)));
+  request.on('data', function(data){
+    if(request.method === "POST") {
+      statusCode = 201;
+      postDataToDatabase(JSON.parse(data));
+      console.log(JSON.parse(data), "Data from POST");
+    } else {
+      statusCode = 200;
+    }
+    var headers = defaultCorsHeaders;
+    headers['Content-Type'] = "application/json";
+    response.writeHead(statusCode, headers);
+    getDataFromDatabase(data.roomName,function(returnData){
+      response.end(new Buffer(returnData));
+    });
+  });
+};
+
+var getDataFromDatabase = function(roomName, cb){
+  var returnData = {};
+  db.getAllMessagesInRoom(roomName, function(messages){
+    returnData.messages = messages;
+    db.getAllRooms(function(rooms){
+      returnData.rooms = rooms;
+      console.log(returnData, "Return Data from POST");
+      cb(returnData);
+    });
+  });
+};
+
+var roomExists = function(rooms, roomName){
+  var exists = false;
+  for (var i = 0; i < rooms.length; i++) {
+    if(roomName === rooms[i]){
+      exists = true;
+    }
+  }
+  return exists;
 };
 
 var postDataToDatabase = function(data){
   db.getAllUsers(function(users){
+      // console.log(users[i].userName, "user name from DB");
+    var userExists = false;
     for (var i = 0; i < users.length; i++) {
       if(users[i].userName === data.username){
-        var user = users[i];
-        db.addMessage(user.userId, data.text, user.roomName);
-      } else {
-        db.addUser(data.username, data.roomName);
+        userExists = true;
+        break;
+      }
+    }
+    if(!userExists){
+      db.addUser(data.username, data.roomname);
+    }
+      db.getAllRooms(function(rooms){
+        if(!roomExists(rooms, data.roomname)){
+          db.addRoom(data.roomName);
+        }
         db.getUserId(data.username, function(userId){
           db.addMessage(userId, data.text, data.roomName);
         });
-      }
+      });
     }
   });
 };
+
 
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
